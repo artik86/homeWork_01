03c14397c0f7bc8148743c1512bcf75fa0ca061e
artik86 <70920449+artik86@users.noreply.github.com>
2024-11-01 23:12:05 +0300

    answers.js

---
+++ answers.js
@@ -0,0 +1,16 @@
+const res = "B" + "a" + (1 - "hello");
+console.log(res); // 
+ответ  BaNaN
+
+
+
+const res2 = (true && 3) + "d";
+console.log(res2); //
+
+ответ 3d
+
+
+const res3 = Boolean(true && 3) + "d";
+console.log(res3); //
+
+ответ trued
